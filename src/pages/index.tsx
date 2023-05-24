423f0026ab1ef726ca7d0dffac4b408068f50db1
BuildTools <unconfigured@null.spigotmc.org>
2023-05-24 18:41:05 -0400

    second

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,79 @@
-import { type NextPage } from "next";
-import { useEffect, useRef, useState } from "react";
+import React, { useState } from "react";
 import ReactAudioPlayer from "react-audio-player";
 
-const Home: NextPage = () => {
-  const [music, setMusic] = useState(["./heroes.mp3", "./on.mp3"]);
-  const [count, setCount] = useState(0);
-  console.log(count);
-  const handleVideoEnd = () => {
-    setCount(count + 1);
-    console.log("ended");
-    console.log(count);
-    setMusic(music[count]);
-    console.log(music[count], "2");
+const Home = () => {
+  const [music, setMusic] = useState([
+    { title: "Heroes", src: "./heroes.mp3" },
+    { title: "On", src: "./on.mp3" },
+  ]);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [playlist, setPlaylist] = useState([]);
+
+  const handleAudioEnded = () => {
+    setCurrentTrackIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+      if (nextIndex < playlist.length) {
+        return nextIndex;
+      } else {
+        return 0;
+      }
+    });
   };
+  const handleNextTrack = () => {
+    setCurrentTrackIndex((prevIndex) => {
+      const nextIndex = prevIndex + 1;
+      if (nextIndex < playlist.length) {
+        return nextIndex;
+      } else {
+        return 0;
+      }
+    });
+  };
+
+  const handlePreviousTrack = () => {
+    setCurrentTrackIndex((prevIndex) => {
+      const nextIndex = prevIndex - 1;
+      if (nextIndex >= 0) {
+        return nextIndex;
+      } else {
+        return playlist.length - 1;
+      }
+    });
+  };
+
+  const handleAddToPlaylist = (track) => {
+    setPlaylist((prevPlaylist) => [...prevPlaylist, track]);
+  };
+
   return (
     <div>
       <ReactAudioPlayer
-        src={music[count]}
+        src={music[currentTrackIndex].src}
         autoPlay
         controls
-        onEnded={handleVideoEnd}
+        onEnded={handleAudioEnded}
       />
-      <ReactAudioPlayer controls>
-        <source src="./heroes.mp3" type="audio/mp3" />
-        <source src="./on.mp3" type="audio/mp3" />
-      </ReactAudioPlayer>
+      <button onClick={handlePreviousTrack}>Previous Track</button>
+      <button onClick={handleNextTrack}>Next Track</button>
+      <ul>
+        {music.map((track, index) => (
+          <li key={track.src}>
+            <button onClick={() => handleAddToPlaylist(track)}>
+              {track.index, track.title}
+            </button>
+          </li>
+        ))}
+      </ul>
+
+      <ul>
+        {playlist.map((track, index) => (
+          <li key={track.src}>
+            <button onClick={() => setCurrentTrackIndex(index)}>
+              {track.title}
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
