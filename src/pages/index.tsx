f0fbe6284d83cf563e05bf149b12bb9612e418dd
BuildTools <unconfigured@null.spigotmc.org>
2023-05-25 09:31:21 -0400

    made the player functional, can now add and remove desired songs from playlist

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,25 +1,29 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import ReactAudioPlayer from "react-audio-player";
 
 const Home = () => {
   const [music, setMusic] = useState([
     { title: "Heroes", src: "./heroes.mp3" },
     { title: "On", src: "./on.mp3" },
+    { title: "Hellcat", src: "./hellcat.mp3" },
+    { title: "SkyHigh", src: "./skyhigh.mp3" },
+    { title: "WhyWeLose", src: "./welose.mp3" },
   ]);
-  const [currentTrackIndex, setCurrentTrackIndex] = useState(0);
+  const [currentTrackIndex, setCurrentTrackIndex] = useState(-1);
   const [playlist, setPlaylist] = useState([]);
+  const [playerReady, setPlayerReady] = useState(false);
+
+  useEffect(() => {
+    const filteredPlaylist = playlist.reduce((acc, current) => {
+     return !acc.find((item) => item.src === current.src) ? acc.concat([current]) : acc;
+    }, []);
+    setPlaylist(filteredPlaylist);
+    if (playlist.length > 0 && currentTrackIndex === -1) {
+      setCurrentTrackIndex(0);
+    }
+  }, [playlist]);
 
   const handleAudioEnded = () => {
-    setCurrentTrackIndex((prevIndex) => {
-      const nextIndex = prevIndex + 1;
-      if (nextIndex < playlist.length) {
-        return nextIndex;
-      } else {
-        return 0;
-      }
-    });
-  };
-  const handleNextTrack = () => {
     setCurrentTrackIndex((prevIndex) => {
       const nextIndex = prevIndex + 1;
       if (nextIndex < playlist.length) {
@@ -42,34 +46,62 @@ const Home = () => {
   };
 
   const handleAddToPlaylist = (track) => {
-    setPlaylist((prevPlaylist) => [...prevPlaylist, track]);
+    if (track.title !== "Heroes") {
+      setPlaylist((prevPlaylist) => [...prevPlaylist, track]);
+      if (currentTrackIndex === -1) {
+        setCurrentTrackIndex(0);
+      }
+    }
+  };
+
+  const handlePlayerReady = () => {
+    setPlayerReady(true);
   };
 
   return (
     <div>
       <ReactAudioPlayer
-        src={music[currentTrackIndex].src}
+        className="mx-auto block w-1/2"
+        style={{  }}
+        src={playerReady ? playlist[currentTrackIndex]?.src : "./heroes.mp3"}
         autoPlay
         controls
         onEnded={handleAudioEnded}
+        onCanPlay={handlePlayerReady}
       />
-      <button onClick={handlePreviousTrack}>Previous Track</button>
-      <button onClick={handleNextTrack}>Next Track</button>
+      <button className="mb-6" onClick={handlePreviousTrack}>
+        Previous Track
+      </button>
+      <button className="mx-12" onClick={handleAudioEnded}>
+        Next Track
+      </button>
       <ul>
         {music.map((track, index) => (
           <li key={track.src}>
-            <button onClick={() => handleAddToPlaylist(track)}>
-              {track.index, track.title}
+            <button
+              className="mx-auto block px-3"
+              onClick={() => handleAddToPlaylist(track)}
+            >
+              {index} {track.title}
             </button>
           </li>
         ))}
       </ul>
-
+      <br />
       <ul>
         {playlist.map((track, index) => (
-          <li key={track.src}>
-            <button onClick={() => setCurrentTrackIndex(index)}>
+          <li key={index}>
+            <button
+              className={
+                index === currentTrackIndex
+                  ? "mx-auto block w-1/2 text-blue-500"
+                  : "mx-auto block w-1/2"
+              }
+              onClick={() => setCurrentTrackIndex(index)}
+            >
+              {index} &nbsp; &times; &nbsp;
               {track.title}
+              {index === currentTrackIndex ? " (Now Playing)" : ""}
             </button>
           </li>
         ))}
